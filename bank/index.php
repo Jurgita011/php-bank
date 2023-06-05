ada66a34f5977b8d63313d5ca4df4bfcdf908771
Jurgita <jurgita011@gmail.com>
2023-06-05 07:44:05 +0300

    initial

---
+++ bank/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Banko aplikacija </title>
+</head>
+<body style="background:#DF00FF;">
+    <h1>Sąskaitų sąrašas</h1>
+    <table>
+        <tr>
+            <th>Vardas</th>
+            <th>Pavardė</th>
+            <th>Sąskaitos numeris</th>
+            <th>Veiksmai</th>
+        </tr>
+        <?php
+        // Čia įterpiamas kodas, kuris nuskaito sąskaitas iš duomenų bazės ir jas atvaizduoja
+        ?>
+    </table>
+    <br>
+    <a href="account.php">Sukurti naują sąskaitą</a>
+</body>
+</html>
